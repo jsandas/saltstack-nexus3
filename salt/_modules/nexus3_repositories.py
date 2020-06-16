b343d27f1f65eadc366af29a074fdb0c754596f5
jsandas <jsandas82@gmail.com>
2020-06-15 23:04:42 -0600

    added create_hosted function

--- salt/_modules/nexus3_repositories.py
+++ salt/_modules/nexus3_repositories.py
@@ -118,6 +118,198 @@ def list_all():
     return ret
 
 
+def create_hosted(name,
+                repo_format,
+                blobstore='default',
+                write_policy='allow_once',
+                cleanup_policies=[],
+                apt_dist_name='bionic',
+                apt_gpg_priv_key='',
+                apt_gpg_passphrase='',
+                docker_v1_enabled=False,
+                docker_force_auth=True,
+                docker_http_port=None,
+                docker_https_port=None,
+                maven_version_policy='MIXED',
+                maven_layout_policy='STRICT',
+                yum_repodata_depth=0,
+                yum_deploy_policy='STRICT',
+                strict_content_validation=True,
+                **kwargs):
+
+    '''
+    Nexus 3 supports many different formats.  The apt, bower, docker, maven, and nuget formats have built-in arguments.
+    Parameters for other formats may be supplied as a dictionary as kwargs.  This would be useful for any new formats
+    add to Nexus.
+
+    name (str):
+        Name of repository
+    
+    repo_format (str):
+        Format of repository [apt|bower|cocoapads|conan|docker|etc.]
+        .. note::
+            This can be any officaly supported repository format for Nexus
+
+    blobstore (str):
+        Name of blobstore to use (Default: default)
+
+    write_policy (str):
+        Controls if deployments of and updates to artifacts are allowed [allow|allow_once|deny] (Default: allow)
+
+    cleanup_policies (list):
+        List of cleanup policies to apply to repository (Default: [])
+
+    content_max_age (int):
+        Max age of content cache in seconds (Default: 1440)
+
+    apt_dist_name (str):
+        Apt distribution name (Default: bionic)
+
+    apt_gpg_priv_key (str):
+        GPG signing private key (Default: '')
+        .. note::
+            This is require for hosted apt repositories
+
+    apt_gpg_passphrase (str):
+        GPG signing private key passphrase (Default: '')
+
+    docker_v1_enabled (bool):
+        Enable v1 api support [True|False] (Default: False)
+
+    docker_force_auth (bool):
+        Force basic authentication [True|False] (Default: True)
+
+    docker_http_port (int):
+        HTTP port for docker api (Default: None)
+        .. note::
+            Normally used if the server is behind a secure proxy
+
+    docker_https_port (int):
+        HTTPS port for docker api (Default: None)
+        .. note::
+            Normally used if the server is configured for https
+
+    maven_version_policy (str):
+        Type of marven artificats this repository stores [RELEASE|SNAPSHOT|MIXED] (default: MIXED)
+
+    maven_layout_policy (str):
+        Validate all paths are maven artifacts or metadata paths [STRICT|PERMISSIVE] (default: STRICT)
+
+    yum_repodata_depth (int):
+        Specifies the repository depth where repodata folder(s) are created (Default: 0)
+
+    yum_deploy_policy (str):
+        Validate that all paths are RPMs or yum metadata [STRICT|PERMISSIVE] (Default: STRICT)
+
+    strict_content_validation (bool):
+        Enable strict content type validation [True|False] (Default: True)
+    
+    kwargs (dict):
+        Any additional parameters for specific repository formats passed as a dictionary. 
+        Check the Nexus 3 API docs for more information on other formats.
+        .. example::
+            yum='{'repodataDepth': 0, 'deployPolicy': 'STRICT'}
+        .. note::
+            The above example is for reference.  Yum is fully supported with arguments in this function.
+  }
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt myminion nexus3.repositories.create_hosted name=test-raw repo_format=raw blobstore=raw_blobstore
+
+        salt myminion nexus3_repositories.create_hosted name=test-yum repo_format=yum yum_repodata_depth=3 yum_deploy_policy=permissive
+    '''
+
+    ret = {
+        'repository': {},
+    }
+
+    payload = {
+        'name': name,
+        'online': True,
+        'storage': {
+            'blobStoreName': blobstore,
+            'strictContentTypeValidation': strict_content_validation,
+            'writePolicy': write_policy
+        },
+        'cleanup': {
+            'policyNames': cleanup_policies
+        }
+    }
+
+    apt = {
+        'apt': {
+            'distribution': apt_dist_name,
+        },
+        'aptSigning': {
+            'keypair': apt_gpg_priv_key
+        }
+    }
+
+    docker = {
+        'docker': {
+            'v1Enabled': docker_v1_enabled,
+            'forceBasicAuth': docker_force_auth,
+        }
+    }
+
+    maven = {
+        'maven': {
+            'versionPolicy': maven_version_policy.upper(),
+            'layoutPolicy': maven_layout_policy.upper()
+        }
+    }
+
+    yum = {
+        'yum': {
+            'repodataDepth': yum_repodata_depth,
+            'deployPolicy': yum_deploy_policy.upper()
+        }
+    }
+
+    if repo_format == 'apt':
+        if apt_gpg_passphrase != '':
+            apt['aptSigning']['passphrase'] = apt_gpg_passphrase
+        payload.update(apt)
+
+    if repo_format == 'docker':
+        if docker_http_port is not None:
+            docker['docker']['httpPort'] = docker_http_port
+        if docker_https_port is not None:
+            docker['docker']['httpsPort'] = docker_https_port
+        payload.update(docker)
+    
+    if repo_format == 'maven':
+        payload.update(maven)
+
+    if repo_format == 'yum':
+        payload.update(yum)
+
+    if kwargs:
+        payload.update(kwargs)
+
+    metadata = describe(name)
+
+    if metadata['repository']:
+        ret['comment'] = 'Repository {} already exists.'.format(name)
+        return ret
+
+    create_path = repo_beta_path + '/' + repo_format + '/hosted'
+
+    nc = nexus3.NexusClient()
+    resp = nc.post(create_path, payload)
+
+    if resp['status'] == 201:
+        ret['repository'] = describe(name)['repository']
+    else:
+        ret['comment'] = 'Failed to create repository {}.'.format(name)
+        ret['error'] = 'code:{} msg:{}'.format(resp['status'], resp['body'])
+
+    return ret
+
+
 def create_proxy(name,
                 repo_format,
                 remote_url,
@@ -282,15 +474,15 @@ def create_proxy(name,
 
     docker = {
         'dockerProxy': {
-            'indexType': docker_index_type,
+            'indexType': docker_index_type.upper(),
             'indexUrl': docker_index_url
         }
     }
 
     maven = {
         'maven': {
-            'versionPolicy': maven_version_policy,
-            'layoutPolicy': maven_layout_policy
+            'versionPolicy': maven_version_policy.upper(),
+            'layoutPolicy': maven_layout_policy.upper()
         }
     }
 
