77975bb5151232998225c4660e0ec9e99afeeedc
jsandas <jsandas82@gmail.com>
2020-07-26 16:19:00 -0600

    added comparison for updates

--- salt/_states/nexus3_repositories.py
+++ salt/_states/nexus3_repositories.py
@@ -66,8 +66,8 @@ def absent(name):
 
 
 def present(name,
-        repo_format,
-        repo_type,
+        format,
+        type,
         apt_dist_name='bionic',
         apt_flat_repo=False,
         apt_gpg_passphrase='',
@@ -99,12 +99,12 @@ def present(name,
         name (str):
             name of respository
 
-    repo_format (str):
+    format (str):
         Format of repository [apt|bower|cocoapads|conan|docker|maven2|etc.]
         .. note::
             This can be any officaly supported repository format for Nexus
 
-    repo_type (str):
+    type (str):
         Repository type [hosted|group|proxy]
 
     apt_dist_name (str):
@@ -139,12 +139,12 @@ def present(name,
     docker_http_port (int):
         HTTP port for docker api (Default: None)
         .. note::
-            Normally used if the server is behind a secure proxy
+            Used if the server is behind a secure proxy
 
     docker_https_port (int):
         HTTPS port for docker api (Default: None)
         .. note::
-            Normally used if the server is configured for https
+            Used if the server is configured for https
 
     docker_index_type (str):
         Type of index for docker registry [REGISTRY|HUB|CUSTOM] (Default: HUB)
@@ -196,7 +196,7 @@ def present(name,
         create_repository:
           nexus3_repositories.present:
             - name: test-yum
-            - repo_type: proxy
+            - type: proxy
             - blobstore: yum-blobstore
             - remote_url: https://yum.example.com
 
@@ -212,40 +212,145 @@ def present(name,
     exists = True
     meta = __salt__['nexus3_repositories.describe'](name=name)
 
-    if not meta['repository']:
+    if 'error' in meta.keys():
+        ret['result'] = False
+        ret['comment'] = meta['error']
+        return ret
+    
+    repo = meta['repository']
+    is_update = False
+    updates = {}
+
+    if repo == {}:
         exists = False
 
+    if exists:
+        if type != repo['type'] or format != repo['format']:
+            ret['result'] = False
+            ret['comment'] = 'repository type and format cannot modified'
+            return ret
+        if blobstore != repo['storage']['blobStoreName']:
+            updates['blobstore'] = blobstore
+            is_update = True
+        if strict_content_validation != repo['storage']['strictContentTypeValidation']:
+            updates['strict_content_validation'] = strict_content_validation
+            is_update = True
 
-    if __opts__['test']:
-        ret['result'] = None
-        ret['comment'] = ''
-
-        if not meta['repository']:            
-            ret['comment'] = 'repository {} will be created. Type: {} Format: {}'.format(name, repo_type, repo_format)
-        else:
-            ret['comment'] = 'repository {} will be updated. Type: {} Format: {}'.format(name, repo_type, repo_format)
-        return ret
-
-    if repo_type == 'group':
+    if type == 'group':
         if exists:
-            is_update = False
-            updates = {}
+            if group_members != repo['group']['memberNames']:
+                updates['group_members'] = group_members
+                is_update = True
 
+            if format == 'docker':
+                if docker_force_auth != repo['docker']['forceBasicAuth']:
+                    updates['docker_force_auth'] = docker_force_auth
+                    is_update = True
+                if docker_v1_enabled != repo['docker']['v1Enabled']:
+                    updates['docker_v1_enabled'] = docker_v1_enabled
+                    is_update = True
+                if docker_http_port != repo['docker']['httpPort']:
+                    updates['docker_http_port'] = docker_http_port
+                    is_update = True
+                if docker_https_port != repo['docker']['httpsPort']:
+                    updates['docker_https_port'] = docker_https_port
+                    is_update = True
+
+        if __opts__['test']:
+            if exists:            
+                if is_update:
+                    ret['result'] = None
+                    ret['comment'] = 'repository {} will be updated with: {}'.format(name, updates)
+                else:
+                    ret['comment'] = 'repository {} is in desired state'.format(name)
+            else:
+                ret['result'] = None
+                ret['comment'] = 'repository {} will be created. Type: {} Format: {}'.format(name, type, format)
+            return ret
+
+        if exists and not is_update:      
+            ret['comment'] = 'repository {} is in desired state'.format(name)
+            return ret
 
         resp = __salt__['nexus3_repositories.group'](name,
-                                                repo_format,
+                                                format,
                                                 blobstore,
                                                 docker_force_auth,
                                                 docker_http_port,
                                                 docker_https_port,
                                                 docker_v1_enabled,
                                                 group_members,
-                                                strict_content_validation,
-                                                **kwargs)
+                                                strict_content_validation)
 
-    if repo_type == 'hosted':
+    if type == 'hosted':
+        if exists:
+            if repo['cleanup'] is not None and cleanup_policies != repo['cleanup']['policyNames']:
+                updates['cleanup_policies'] = cleanup_policies
+                is_update = True
+            if write_policy != repo['storage']['writePolicy']:
+                updates['write_policy'] = write_policy
+                is_update = True
+
+            if format == 'apt':
+                if apt_dist_name != repo['apt']['distribution']:
+                    updates['apt_dist_name'] = apt_dist_name
+                    is_update = True
+                # not sure if this is returned when describing a repo
+                # if apt_gpg_passphrase != repo['aptSigning']['passphrase']:
+                #     updates['apt_gpg_passphrase'] = apt_gpg_passphrase
+                #     is_update = True
+                if apt_gpg_priv_key != repo['aptSigning']['keypair']:
+                    updates['apt_gpg_priv_key'] = apt_gpg_priv_key
+                    is_update = True
+
+            if format == 'docker':
+                if docker_force_auth != repo['docker']['forceBasicAuth']:
+                    updates['docker_force_auth'] = docker_force_auth
+                    is_update = True
+                if docker_v1_enabled != repo['docker']['v1Enabled']:
+                    updates['docker_v1_enabled'] = docker_v1_enabled
+                    is_update = True
+                if docker_http_port != repo['docker']['httpPort']:
+                    updates['docker_http_port'] = docker_http_port
+                    is_update = True
+                if docker_https_port != repo['docker']['httpsPort']:
+                    updates['docker_https_port'] = docker_https_port
+                    is_update = True
+
+            if format == 'maven2':
+                if maven_layout_policy != repo['maven']['layoutPolicy']:
+                    updates['maven_layout_policy'] = maven_layout_policy
+                    is_update = True
+                if maven_version_policy != repo['maven']['versionPolicy']:
+                    updates['maven_version_policy'] = maven_version_policy
+                    is_update = True                
+
+            if format == 'yum':
+                if yum_deploy_policy != repo['yum']['deployPolicy']:
+                    updates['yum_deploy_policy'] = yum_deploy_policy
+                    is_update = True                          
+                if yum_repodata_depth != repo['yum']['repodataDepth']:
+                    updates['yum_repodata_depth'] = yum_repodata_depth
+                    is_update = True
+
+        if __opts__['test']:
+            if exists:            
+                if is_update:
+                    ret['result'] = None
+                    ret['comment'] = 'repository {} will be updated with: {}'.format(name, updates)
+                else:
+                    ret['comment'] = 'repository {} is in desired state'.format(name)
+            else:
+                ret['result'] = None
+                ret['comment'] = 'repository {} will be created. Type: {} Format: {}'.format(name, type, format)
+            return ret
+
+        if exists and not is_update:      
+            ret['comment'] = 'repository {} is in desired state'.format(name)
+            return ret
+
         resp = __salt__['nexus3_repositories.hosted'](name,
-                                                repo_format,
+                                                format,
                                                 apt_dist_name,
                                                 apt_gpg_passphrase,
                                                 apt_gpg_priv_key,
@@ -260,29 +365,98 @@ def present(name,
                                                 strict_content_validation,
                                                 yum_deploy_policy,
                                                 yum_repodata_depth,
-                                                write_policy,
-                                                **kwargs)
+                                                write_policy)
 
-    if repo_type == 'proxy':
+    if type == 'proxy':
+        if exists:
+            if remote_url != repo['proxy']['remoteUrl']:
+                updates['remote_url'] = remote_url
+                is_update = True
+            if repo['cleanup'] is not None and cleanup_policies != repo['cleanup']['policyNames']:
+                updates['cleanup_policies'] = cleanup_policies
+                is_update = True
+            if content_max_age != repo['proxy']['contentMaxAge']:
+                updates['content_max_age'] = content_max_age
+                is_update = True
+            if metadata_max_age != repo['proxy']['metadataMaxAge']:
+                updates['metadata_max_age'] = metadata_max_age
+                is_update = True
+            # cannot compare username as there is no way to determine if the same
+            # if remote_password != repo['httpClient']['authentication']['password']:
+            #     updates['remote_password'] = remote_password
+            #     is_update = True
+            if remote_username != repo['httpClient']['authentication']['username']:
+                updates['remote_username'] = remote_username
+                is_update = True
+
+            if format == 'apt':
+                if apt_dist_name != repo['apt']['distribution']:
+                    updates['apt_dist_name'] = apt_dist_name
+                    is_update = True
+                if apt_flat_repo != repo['flat']:
+                    updates['apt_flat_repo'] = apt_flat_repo
+                    is_update = True
+
+            if format == 'bower':
+                if bower_rewrite_urls != repo['bower']['rewritePackageUrls']:
+                    updates['bower_rewrite_urls'] = bower_rewrite_urls
+                    is_update = True                
+
+            if format == 'docker':
+                if docker_index_type != repo['dockerProxy']['indexType']:
+                    updates['docker_index_type'] = docker_index_type
+                    is_update = True
+                if docker_index_url != repo['dockerProxy']['indexUrl']:
+                    updates['docker_index_url'] = docker_index_url
+                    is_update = True
+
+            if format == 'maven2':
+                if maven_layout_policy != repo['maven']['layoutPolicy']:
+                    updates['maven_layout_policy'] = maven_layout_policy
+                    is_update = True
+                if maven_version_policy != repo['maven']['versionPolicy']:
+                    updates['maven_version_policy'] = maven_version_policy
+                    is_update = True   
+
+            if format == 'nuget':
+                if nuget_cache_max_age != repo['nugetProxy']['queryCacheItemMaxAge']:
+                    updates['nuget_cache_max_age'] = nuget_cache_max_age
+                    is_update = True
+
+        if __opts__['test']:
+            if exists:            
+                if is_update:
+                    ret['result'] = None
+                    ret['comment'] = 'repository {} will be updated with: {}'.format(name, updates)
+                else:
+                    ret['comment'] = 'repository {} is in desired state'.format(name)
+            else:
+                ret['result'] = None
+                ret['comment'] = 'repository {} will be created. Type: {} Format: {}'.format(name, type, format)
+            return ret
+
+        if exists and not is_update:      
+            ret['comment'] = 'repository {} is in desired state'.format(name)
+            return ret
+
         resp = __salt__['nexus3_repositories.proxy'](name,
-                                                repo_format,
-                                                remote_url,
-                                                apt_dist_name,
-                                                apt_flat_repo,
-                                                blobstore,
-                                                bower_rewrite_urls,
-                                                cleanup_policies,
-                                                content_max_age,
-                                                docker_index_type,
-                                                docker_index_url,
-                                                maven_layout_policy,
-                                                maven_version_policy,
-                                                metadata_max_age,
-                                                nuget_cache_max_age,
-                                                remote_password,
-                                                remote_username,
-                                                strict_content_validation,
-                                                **kwargs)
+                                                    format,
+                                                    remote_url,
+                                                    apt_dist_name,
+                                                    apt_flat_repo,
+                                                    blobstore,
+                                                    bower_rewrite_urls,
+                                                    cleanup_policies,
+                                                    content_max_age,
+                                                    docker_index_type,
+                                                    docker_index_url,
+                                                    maven_layout_policy,
+                                                    maven_version_policy,
+                                                    metadata_max_age,
+                                                    nuget_cache_max_age,
+                                                    remote_password,
+                                                    remote_username,
+                                                    strict_content_validation)
 
     if 'error' in resp.keys():
         ret['result'] = False
